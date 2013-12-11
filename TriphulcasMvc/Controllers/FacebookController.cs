8c0c2fd6ab6c159643d40960a031a3925fb9053a
elrute <srcoton@gmail.com>
2013-12-11 08:58:42 +0100

    Updated parameter in query to FBK graph according to FBK's acess token new policy: instead of sending "default access token", we need to use "current logged in user access token"

--- TriphulcasMvc/Controllers/FacebookController.cs
+++ TriphulcasMvc/Controllers/FacebookController.cs
@@ -69,7 +69,7 @@ namespace TriphulcasMvc.Controllers
             try
             {
                 string target = String.IsNullOrEmpty(id) ? "me" : id;
-                var client = new FacebookClient(Resources.Resource1.DefaultAccessToken);
+                var client = new FacebookClient((User as FacebookPrincipal).AccessToken);
                 dynamic result = client.Get(target, new { fields = "name,first_name,id" });
 
                 return Json(new
